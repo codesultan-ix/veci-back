2756a32c3e94c7685c4ac57a78044e4890cac8ea
Sultan IOS <codesultan369@gmail.com>
2021-09-06 17:11:50 +0100

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     * 
+     * 
+     */
+    
+
+
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'John Doe',
+            'email' =>'johndoe@example.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
